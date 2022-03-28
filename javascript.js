3381b54052393973af955743503e621693e460e1
KevinHdezVaz <kevinhdezvaz@gmail.com>
2022-03-28 00:04:14 -0600

    contorl

--- javascript.js
+++ javascript.js
@@ -1,27 +1,14 @@
-//-------------------------
-let sumare = (datos,n)
-=>
-{
-    if(n>0)
-    {
-        let x =datos[n] + sumare(datos,n-1);
-        console.log("x: "+x);
-        return (x)
-    }
-    else{
-        console.log("Ultima llamada sumarec:");
-        return (datos[0])
-        
-    }
-}
+ 
+ 
 
 
 let datos = [23,4,5,177,8,190]
 
-let BtnUno = document.getElementById("btnAccion");
+let BtnUno = document.getElementById( "try" );
+ 
 
-BtnUno.addEventListener('cick',async (e)=>{
-    console.log(alumnoconNombre)
+BtnUno.addEventListener('click',async (e)=>{
+    console.log(sumaArreglo)
 })
 
 
@@ -30,17 +17,25 @@ sumaDos = (valor)=> valor-2;
 
 
 
+
+//REDUCE
 const listaOriginal =[1,2,3,4,5];
 
-const nuevaLista = listaOriginal.map((valor) => valor*2);
-const nuevaListaRedu = listaOriginal.map(SumaDos);
+//const nuevaLista = listaOriginal.map((valor) => valor*2);
+//const nuevaListaRedu = listaOriginal.map(SumaDos);
+
+let sumaArreglo = listaOriginal.reduce((acc,item)=>  acc + item,0)
+suma=0;
+ 
+//////////////////////////
+
 
 const alumnos =[
 
-{nombre :'juan', apellidos : 'merino diaz', matricula: 111 , edad: 20},
-{nombre :'rafael', apellidos : 'nava diaz', matricula: 323, edad: 12},
-{nombre :'antonio', apellidos : 'merino gonzaes', matricula: 233 , edad: 40},
-{nombre :'hiperb', apellidos : 'mhernandesz rehgio', matricula: 531 , edad: 22},
+{nombre :'juan', apellidos : 'merino diaz', matricula: 111 , edad: 20,op:"estudiante" },
+{nombre :'rafael', apellidos : 'nava diaz', matricula: 323, edad: 12,op:"estudiante"},
+{nombre :'antonio', apellidos : 'merino gonzaes', matricula: 233 , edad: 40,op:"estudiante"},
+{nombre :'hiperb', apellidos : 'mhernandesz rehgio', matricula: 531 , edad: 22,op:"profesor"},
 
 ]
 
@@ -57,11 +52,26 @@ const alumnoconotroA = alumnos.map(item =>{
         revisado: 'ok'
     }
 })
+///REDUCE CON OBJETOS
+const listaprofes = alumnos.filter (item => item.op =="profesor");
+
+let sumaprofe = alumnos.reduce((acc,item)=> 
+{
+ if(item.op==="profesor")
+    return acc+1
+    else return acc
+}  
+ ,0)
+
+ console.log("la suma es de profes "+sumaprofe);
+
+ //////////////////////
+
 
 //const alumnoconMasEdad = alumnosConEdad
 
-const mensaje_saludar = saludar("roberto")
-mensaje_saludar("bienvenido")
+//const mensaje_saludar = saludar("roberto")
+//mensaje_saludar("bienvenido")
 
 //---------------------------
 function saludar (nombre){
